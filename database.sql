c70d6dcff2b5dc34abacf096b004c55629898d51
Matthew Bouc <boucgm@gmail.com>
2021-06-29 21:10:34 -0500

    database setup

--- database.sql
+++ database.sql
@@ -1,5 +1,19 @@
-CREATE TABLE "artists" (
+CREATE TABLE artist (
     "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+    "name" varchar(80) not null,
+    "birthdate" date
 );
+
+INSERT INTO artist ("name", "birthdate")
+	VALUES ('Ella Fitzgerald', '04-25-1917'), ('Dave Brubeck', '12-06-1920'), ('Miles Davis', '05-26-1926'), ('Esperanza Spalding', '10-18-1984');
+	
+
+CREATE TABLE song (
+	"id" SERIAL PRIMARY KEY,
+	"title" varchar(255) not null,
+	"length" varchar(10) not null,
+	"released" date
+);
+
+INSERT INTO song (title, length, released)
+	VALUES ('Take Five','5:24','1959-09-29'), ('So What','9:22','1959-08-17'), ('Black Gold','5:17','2012-02-01');
